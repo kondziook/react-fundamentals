4f5186e57cb05d51d5739cbc45fe7a28b3ef1241
Konrad Krzysztyniak <konrad.krzysztyniak@totaljobsgroup.com>
2016-07-15 15:30:59 +0200

    Nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,52 @@
+var USER_DATA = {
+    name: 'Boom',
+    username: 'pow',
+    image: 'http://lorempixel.com/400/200/sports/'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+    render: function(){
+        return <img src={this.props.imageUrl} style={{width: 100}} />
+    }
+})
+var ProfileLink = React.createClass({
     render: function(){
         return (
-            <div>Hello World</div>
+        <div>
+            <a href={'https://www.github.com/' + this.props.username}>
+                {this.props.username}
+            </a>
+        </div>
+        )
+    }
+})
+
+var ProfileName = React.createClass({
+    render: function(){
+        return (
+        <div>
+            {this.props.name}            
+        </div>
+        )
+    }
+})
+
+var Avatar = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileLink username={this.props.user.name} />
+                <ProfileName name={this.props.user.username} />
+            </div>
         )
     }
 })
 
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 )
